69ec127e7d3608282cdc0a8b5f44642e3c9989e9
Gustavo Barbosa <gustavocsb@gmail.com>
2014-08-08 10:56:22 -0300

    Code style.

--- Pod/Classes/TNTKeychain.m
+++ Pod/Classes/TNTKeychain.m
@@ -6,7 +6,8 @@
 
 @implementation TNTKeychain
 
-+ (NSMutableDictionary *)getKeychainQuery:(NSString *)service {
++ (NSMutableDictionary *)getKeychainQuery:(NSString *)service
+{
     return [NSMutableDictionary dictionaryWithObjectsAndKeys:
             (__bridge id)kSecClassGenericPassword, (__bridge id)kSecClass,
             service, (__bridge id)kSecAttrService,
@@ -15,27 +16,36 @@
             nil];
 }
 
-+ (void)save:(NSString *)service data:(id)data {
++ (void)save:(NSString *)service data:(id)data
+{
     NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
     SecItemDelete((__bridge CFDictionaryRef)keychainQuery);
-    [keychainQuery setObject:[NSKeyedArchiver archivedDataWithRootObject:data] forKey:(__bridge id)kSecValueData];
+    [keychainQuery setObject:[NSKeyedArchiver archivedDataWithRootObject:data]
+                      forKey:(__bridge id)kSecValueData];
     SecItemAdd((__bridge CFDictionaryRef)keychainQuery, NULL);
 }
 
-+ (id)load:(NSString *)service {
++ (id)load:(NSString *)service
+{
     if (!service)
         return nil;
     
     id ret = nil;
     NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
-    [keychainQuery setObject:(id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
-    [keychainQuery setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
+    [keychainQuery setObject:(id)kCFBooleanTrue
+                      forKey:(__bridge id)kSecReturnData];
+    [keychainQuery setObject:(__bridge id)kSecMatchLimitOne
+                      forKey:(__bridge id)kSecMatchLimit];
+    
     CFDataRef keyData = NULL;
-    if (SecItemCopyMatching((__bridge CFDictionaryRef)keychainQuery, (CFTypeRef *)&keyData) == noErr) {
-        @try {
+    if (SecItemCopyMatching((__bridge CFDictionaryRef)keychainQuery, (CFTypeRef *)&keyData) == noErr)
+    {
+        @try
+        {
             ret = [NSKeyedUnarchiver unarchiveObjectWithData:(__bridge NSData *)keyData];
         }
-        @catch (NSException *e) {
+        @catch (NSException *e)
+        {
             NSLog(@"Unarchive of %@ failed: %@", service, e);
         }
         @finally {}
@@ -44,7 +54,8 @@
     return ret;
 }
 
-+ (void)delete:(NSString *)service {
++ (void)delete:(NSString *)service
+{
     NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
     SecItemDelete((__bridge CFDictionaryRef)keychainQuery);
 }
